2c24cb279a45da281a0366366f99a85bbfc34ee6
hey-Zayn <zaynobusiness@gmail.com>
2025-12-03 13:14:45 +0500

    changed Lib

--- frontend/src/lib/axios.ts
+++ frontend/src/lib/axios.ts
@@ -2,5 +2,5 @@ import axios from "axios"
 
 export const axiosInstance = axios.create({
     // baseURL: import.meta.env.MODE === "development" ? "http://localhost:5000/api" : "/api",
-    baseURL: import.meta.env.MODE === "development" ? "https://music-bd.vercel.app/api" : "/api",
+    baseURL:'https://music-bd.vercel.app/api',
 })
